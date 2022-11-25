de27812e719357a41ed506b8429afc770fa251be
JotaPeAyres <jp.ayres2601@gmail.com>
2022-11-24 21:05:34 -0300

    arrumando imagens

--- src/assets/components/Corpo/index.tsx
+++ src/assets/components/Corpo/index.tsx
@@ -1,25 +1,26 @@
 import './styles.css'
-
-
+import oprojeto from '../../../img/oprojeto.png'
+import oquefazemos from '../../../img/oprojeto.png'
+import ondeestamos from '../../../img/oprojeto.png'
 
 function Corpo(){
     return(
         <section>
-            <div className='corpo'>
+            <div className='corpo'> 
                 <div className='conteudo' >
                     <h3 className='textoPrincipal'>Há uma preocupação constante do Unisagrado com os resíduos produzidos em todas as suas atividades, porém nem todos têm consciência do que nós já fazemos. Esta preocupação com os resíduos vem de uma necessidade não só da instituição, mas um cuidado com os impactos causados no meio ambiente e com os efeitos a curto e longo prazo.</h3>
                 </div>
                 <div className='botoes'>
                     <a href="src\oprojeto.html" target="_blank">
-                        <img className='imgBotoes' src="src/img/oprojeto.png" height="150" width="200"></img>
+                        <img className='imgBotoes' src={oprojeto} height="150" width="200"></img>
                     </a>
 
                     <a href="src\oquefazemos.html" target="_blank">
-                        <img className='imgBotoes' src="src/img/oquefazemos.png" height="150" width="200"></img>
+                        <img className='imgBotoes' src={oquefazemos} height="150" width="200"></img>
                     </a>
                     
                     <a href="src\credito.html" target="_blank">
-                        <img className='imgBotoes' src="src/img/ondeestamos.png" height="150" width="200"></img>
+                        <img className='imgBotoes' src={ondeestamos} height="150" width="200"></img>
                     </a>
                 </div>
             </div>
